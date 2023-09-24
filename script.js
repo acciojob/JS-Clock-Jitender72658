9b5401793d66db878b95c3d9d13695f05fb62f56
Jitender Kumar <111197506+Jitender72658@users.noreply.github.com>
2023-09-24 13:24:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,17 +4,18 @@ function updateClock() {
   const hourHand = document.querySelector('.hour-hand');
   const minuteHand = document.querySelector('.min-hand');
   const secondHand = document.querySelector('.second-hand');
-
+  const time = document.querySelector('#time');
+	
   // Get the current time
   const now = new Date();
   const hours = now.getHours();
   const minutes = now.getMinutes();
   const seconds = now.getSeconds();
-
+  time.innerText = `${hours}: ${minutes}: ${seconds}`;
   // Calculate the degree of rotation for each hand
-  const hourRotation = (360 / 12) * (hours % 12) + (360 / 12) * (minutes / 60);
-  const minuteRotation = (360 / 60) * minutes + (360 / 60) * (seconds / 60);
-  const secondRotation = (360 / 60) * seconds;
+  const hourRotation = (360 / 12) * ((hours+15) % 12) + (360 / 12) * (minutes / 60);
+  const minuteRotation = (360 / 60) * (minutes+15) + (360 / 60) * (seconds / 60);
+  const secondRotation = (360 / 60) * (seconds+15);
 
   // Apply the rotation using the transform property
   hourHand.style.transform = `rotate(${hourRotation}deg)`;
